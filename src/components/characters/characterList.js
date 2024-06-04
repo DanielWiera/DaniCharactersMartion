8694a9eb6baf96d28e3acd01210eab1374b59822
DanielWiera <136990762+DanielWiera@users.noreply.github.com>
2024-06-04 17:29:57 +0200

    Update characterList.js

--- src/components/characters/characterList.js
+++ src/components/characters/characterList.js
@@ -37,7 +37,7 @@ const characterList = [
         birthday: "2002-02-10",
         img: "morgosrajmund.png",
         pic: "River Viiperi",
-        color: "#3A67CE"
+        color: "#FBE7A1"
     },
     {
         name: "Orsay Rafael",
